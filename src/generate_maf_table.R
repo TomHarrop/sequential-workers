413ea50c99eeecde2fdfbfe8032b7df295878a4d
Tom Harrop <twharrop@gmail.com>
2020-05-28 15:05:03 +1200

    try filtering

--- src/generate_maf_table.R
+++ src/generate_maf_table.R
@@ -13,14 +13,15 @@ library(data.table)
 
 vcf_file <- snakemake@input[["vcf"]]
 
-
 # read the vcf
 vcf_names <- c("#CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO", 
                "FORMAT")
 
+message(paste(date(), "Parsing header"))
 cn <- fread(cmd = paste("zcat", vcf_file, '| grep "^#[^#]" | head -n 1'),
       sep = "\t", header = FALSE)
 
+message(paste(date(), "Reading VCF"))
 vcf <- fread(cmd = paste("zcat", vcf_file, '| grep -v "^#"'),
       sep = "\t",
       header = FALSE,
@@ -28,6 +29,7 @@ vcf <- fread(cmd = paste("zcat", vcf_file, '| grep -v "^#"'),
 
 
 # extract the AO and RO fields
+message(paste(date(), "Converting to long data.table"))
 indiv_cols <- names(vcf)[!names(vcf) %in% vcf_names]
 gt_format <- vcf[1, strsplit(FORMAT, ":", fixed = TRUE)][, V1]
 vcf_long <- melt(vcf,
@@ -35,6 +37,7 @@ vcf_long <- melt(vcf,
      measure.vars = indiv_cols,
      variable.name = "indiv",
      value.name = "genotype_field")
+message(paste(date(), "Parsing the GT field"))
 vcf_split <- vcf_long[, tstrsplit(genotype_field,
                      split = ":",
                      fixed = TRUE),
@@ -42,11 +45,15 @@ vcf_split <- vcf_long[, tstrsplit(genotype_field,
 setnames(vcf_split, paste0("V", 1:7), gt_format)
 
 # calculate MAF
+message(paste(date(), "Calculating MAF"))
+vcf_split[, AO := as.numeric(AO)]
+vcf_split[, RO := as.numeric(RO)]
 vcf_split[, 
-    maf := min(as.numeric(AO), as.numeric(RO)) / (as.numeric(AO) + as.numeric(RO)),
+    maf := min(AO, RO) / (AO + RO),
     by = c("indiv", "#CHROM", "POS", "ID")]
 
 # convert for export
+message(paste(date(), "Converting back to wide data.table"))
 maf_dt <- dcast(vcf_split, `#CHROM` + POS ~ indiv,
                 value.var = "maf")
 maf_dt[, loc_id := paste(`#CHROM`, POS, sep = "_")]
@@ -54,6 +61,7 @@ maf_mat <- t(as.matrix(maf_dt[, c("loc_id", indiv_cols), with = FALSE],
           rownames = "loc_id"))
 maf_dt[, loc_id := NULL]
 
+message(paste(date(), "Writing MAF for", dim(maf_dt)[[1]], "loci"))
 saveRDS(maf_mat, snakemake@output[["maf_mat"]])
 fwrite(maf_dt, snakemake@output[["maf_dt"]])
 
